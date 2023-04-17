fc8cf398de7fd82895e3e1ccbea572f7e03e1873
muhammed roshan <roshantac@gmail.com>
2023-04-17 08:13:03 +0530

    first working code

--- motorController.py
+++ motorController.py
@@ -15,10 +15,17 @@ class motorControll:
 		self.rightForward = 16 #yellow
 		self.rightBackward= 20 #orange
 		
-		self.max_pwm_val = 100
-		self.min_pwm_val = 0
+		self.max_pwm_val = 100.0
+		self.min_pwm_val = 0.0
 		
-		self.maxSpeed =100
+		self.motor_rpm = 60.0
+		self.wheel_diameter = 0.065
+		self.wheel_separation = 0.17
+		
+		self.wheel_radius = self.wheel_diameter/2
+		self.circumference_of_wheel = 2*3.14*self.wheel_radius
+		self.maxSpeed = (self.circumference_of_wheel*self.motor_rpm)/60.0
+		
 					
 		GPIO.setup(self.leftEn, GPIO.OUT)
 		GPIO.setup(self.rightEn, GPIO.OUT)
@@ -29,7 +36,9 @@ class motorControll:
 		#pwm init
 		self.pwmL = GPIO.PWM(self.leftEn, 100)
 		self.pwmR = GPIO.PWM(self.rightEn,100)
-		self.stop()
+		self.pwmL.start(0)
+		self.pwmR.start(0)
+		#self.stop()
 	
 
 
@@ -44,8 +53,8 @@ class motorControll:
 	
 	def move(self,leftSpeed, rightSpeed):
 		#calculate PWM value from speed
-		leftSpeedPWM =  max(min((leftSpeed/self.maxSpeed)*self.max_pwm_val, self.max_pwm_val), self.min_pwm_val)
-		rightSpeedPWM =  max(min((rightSpeed/self.maxSpeed)*self.max_pwm_val, self.max_pwm_val), self.min_pwm_val)
+		leftSpeedPWM =  max(min((abs(leftSpeed)/self.maxSpeed)*self.max_pwm_val, self.max_pwm_val), self.min_pwm_val)
+		rightSpeedPWM =  max(min((abs(rightSpeed)/self.maxSpeed)*self.max_pwm_val, self.max_pwm_val), self.min_pwm_val)
 		#change pwm
 		print(" leftSpeedPWM : " , leftSpeedPWM)
 		print(" rightSpeedPWM : " , rightSpeedPWM)
@@ -57,8 +66,8 @@ class motorControll:
 			GPIO.output(self.leftForward, GPIO.HIGH)
 			GPIO.output(self.leftBackward, GPIO.LOW)
 		else:
-			GPIO.output(leftForward, GPIO.LOW)
-			GPIO.output(leftBackward, GPIO.HIGH)
+			GPIO.output(self.leftForward, GPIO.LOW)
+			GPIO.output(self.leftBackward, GPIO.HIGH)
 			
 		if rightSpeed>=0:
 			GPIO.output(self.rightForward, GPIO.HIGH)
@@ -70,9 +79,9 @@ class motorControll:
 
 
 test = motorControll()
-test.move(250, 60)			
-while(1):
-	pass
+test.move(-60, -60)		
+time.sleep(2)
+test.stop()		
 		
 		
 		
